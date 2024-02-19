b10a495cfe129d89ca62dfa6f161a73b2877a202
msaeiid <mskarbaschian@gmail.com>
2024-02-19 23:21:15 +0330

    Some improvement

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -13,18 +13,22 @@ urlpatterns = [
     path('experience/', UpdateView.as_view(), name='experience'),
     path('experience/add', UpdateView.as_view(), name='experience_add'),
     path('experience/update/<int:pk>', UpdateView.as_view(), name='experience_update'),
+    path('experience/delete/<int:pk>', UpdateView.as_view(), name='experience_delete'),
 
     path('certificate/', UpdateView.as_view(), name='certificate'),
     path('certificate/add', UpdateView.as_view(), name='certificate_add'),
-    path('certificate/update/<int:pk>', UpdateView.as_view(), name='experience_update'),
+    path('certificate/update/<int:pk>', UpdateView.as_view(), name='certificate_update'),
+    path('certificate/delete/<int:pk>', UpdateView.as_view(), name='certificate_delete'),
 
     path('education/', UpdateView.as_view(), name='education'),
     path('education/add', UpdateView.as_view(), name='education_add'),
     path('education/update/<int:pk>', UpdateView.as_view(), name='education_update'),
+    path('education/delete/<int:pk>', UpdateView.as_view(), name='education_delete'),
 
     path('skill/', UpdateView.as_view(), name='skill'),
     path('skill/add', UpdateView.as_view(), name='skill_add'),
     path('skill/update/<int:pk>', UpdateView.as_view(), name='skill_update'),
+    path('skill/delete/<int:pk>', UpdateView.as_view(), name='skill_delete'),
 
     path('logout/', LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
 ]
