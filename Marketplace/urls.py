74de27406d47aca4c99a98e5286ee1fc519f8a8f
msaeiid <mskarbaschian@gmail.com>
2024-02-21 22:08:03 +0330

    login has just started

--- Marketplace/urls.py
+++ Marketplace/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from Marketplace.views import Index, Contact
+from Marketplace.views import Index, Contact, ItemDetail
 
 app_name = 'Marketplace'
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('contact/', Contact.as_view(), name='contact'),
+    path('items/<int:pk>', ItemDetail.as_view(), name='item_detail')
 
 ]
--- Marketplace/views.py
+++ Marketplace/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from Marketplace.models import Item, Category
 
 
 class Index(TemplateView):
     def get(self, request, *args, **kwargs):
-        return render(request, template_name='Marketplace/index.html')
+        items = Item.objects.filter(is_sold=False)[0:6]
+        categories = Category.objects.all()
+        context = {
+            'items': items,
+            'categories': categories,
+        }
+        return render(request, template_name='Marketplace/index.html', context=context)
 
 
 class Contact(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, template_name='Marketplace/contact.html')
+
+
+class ItemDetail(DetailView):
+    model = Item
+    template_name = 'Marketplace/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemDetail, self).get_context_data(**kwargs)
+        context['related_items'] = Item.objects.filter(category=context['item'].category, is_sold=False).exclude(
+            pk=context['item'].pk)[0:3]
+        return context
